c864b3efcc0277653eabb7b17bd66ad58bfb8540
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-01-07 19:05:37 +0100

    logging with @Slf4j annotations

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6restmvc.controller;
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 /**
  * Created by robertZ on 2024-01-07.
  */
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
@@ -18,6 +20,9 @@ public class BeerController {
     private final BeerService beerService;
 
     Beer getBeerById(UUID id){
+
+        log.debug("Get Beer By ID - in controller " + id);
+
         return beerService.getBeerById(id);
     }
 }
--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -11,10 +12,14 @@ import java.util.UUID;
 /**
  * Created by robertZ on 2024-01-07.
  */
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("BeerService create Beer by ID " + id);
+
         return Beer.builder()
                 .id(id)
                 .beerName("Hora")
