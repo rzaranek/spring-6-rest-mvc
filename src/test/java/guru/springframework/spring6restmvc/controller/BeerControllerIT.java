5b60095963aa77d3a1d6a27a2a2d73a9f3169da3
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-03-18 19:53:49 +0100

    exception test

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -9,9 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by robertZ on 2024-03-18.
@@ -25,6 +27,23 @@ class BeerControllerIT {
     BeerController beerController;
 
     @Test
+    void testBeerIsNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetBeerById() {
+        //given
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+
+        assertThat(beerDTO).isNotNull();
+    }
+
+    @Test
     void testListBeers() {
         //given
         List<BeerDTO> beerDTOList = beerController.listBeers();
@@ -35,14 +54,10 @@ class BeerControllerIT {
     @Rollback
     @Transactional
     @Test
-    void testEmptyList(){
+    void testEmptyList() {
         beerRepository.deleteAll();
         List<BeerDTO> beerDTOList = beerController.listBeers();
 
         assertThat(beerDTOList.size()).isEqualTo(0);
-    }
-
-    @Test
-    void getBeerById() {
     }
 }
