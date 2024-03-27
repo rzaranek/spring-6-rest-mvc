bc3aed5a0bb93f887470bff299762e5ba18c6830
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-03-27 19:35:31 +0100

    database validation

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,11 +1,16 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by robertZ on 2024-01-31.
@@ -13,16 +18,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class BeerRepositoryTest {
 
-    public static final String MY_BEER_NAME = "My Beer 0%";
+    public static final String MY_BEER_NAME = "My Super Beer 0%";
     @Autowired
     BeerRepository beerRepository;
 
     @Test
-    void createBeerTest(){
+    void createBeerTooLongNameTest() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+
+            Beer beer = Beer.builder()
+                    .beerName("Beer 0123456789+0123456789+0123456789+0123456789+0123456789")
+                    .beerStyle(BeerStyle.PORTER)
+                    .upc("123123132")
+                    .price(BigDecimal.valueOf(9.99))
+                    .build();
+
+            beerRepository.save(beer);
+            beerRepository.flush();
+
+        });
+    }
+
+    @Test
+    void createBeerTest() {
 
         Beer createdBeer = beerRepository.save(Beer.builder()
-                        .beerName(MY_BEER_NAME)
+                .beerName(MY_BEER_NAME)
+                .beerStyle(BeerStyle.PORTER)
+                .upc("123123132")
+                .price(BigDecimal.valueOf(9.99))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(createdBeer).isNotNull();
         assertThat(createdBeer.getBeerName()).isEqualTo(MY_BEER_NAME);
