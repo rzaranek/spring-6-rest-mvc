e5a956752bd46857057e5f7bc26a12e493f6764f
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-07-04 20:45:16 +0200

    parse service

---
+++ src/test/java/guru/springframework/spring6restmvc/services/BeerCsvServiceTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeerCsvServiceTest {
+
+    BeerCsvService beerCsvService = new BeerCsvServiceImpl();
+
+    @Test
+    void convertCSV() throws FileNotFoundException {
+
+        File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+        List<BeerCSVRecord> beers = beerCsvService.convertCSV(file);
+
+        System.out.println(beers.size());
+
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+}
