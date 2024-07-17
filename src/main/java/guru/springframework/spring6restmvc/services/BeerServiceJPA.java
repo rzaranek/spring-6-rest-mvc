40a757fea5dca2da1b38908cc2c2b3ab99a8d96a
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-07-17 22:16:37 +0200

    query list by name

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -44,7 +44,7 @@ public class BeerServiceJPA implements BeerService {
     }
 
     List<Beer> listBeersByName(String beerName) {
-        return new ArrayList<>();
+        return beerRepository.findAllByBeerNameIsLikeIgnoreCase('%' + beerName + '%');
     }
 
     @Override
--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -68,7 +68,7 @@ class BeerControllerIT {
         mockMvc.perform(get(BeerController.BEER_PATH)
                         .queryParam("beerName", "IPA"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", is(2413)));
+                .andExpect(jsonPath("$.size()", is(336)));
     }
 
     @Test
