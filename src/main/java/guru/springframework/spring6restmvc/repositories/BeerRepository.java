1cfeecb0498282cf6b5693fc802da9f7555d3bf9
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-07-17 22:00:29 +0200

    query list by name

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -3,10 +3,14 @@ package guru.springframework.spring6restmvc.repositories;
 import guru.springframework.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by robertZ on 2024-01-31.
  */
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
+
 }
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,13 +1,18 @@
 package guru.springframework.spring6restmvc.repositories;
 
+import guru.springframework.spring6restmvc.boostrap.BootstrapData;
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,11 +21,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * Created by robertZ on 2024-01-31.
  */
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     public static final String MY_BEER_NAME = "My Super Beer 0%";
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerByBeerName(){
+
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void createBeerTooLongNameTest() {
