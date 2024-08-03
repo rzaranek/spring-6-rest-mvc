0bd1c9ecf2b29b0d13eda53e0cc42c2f68a70386
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-08-03 16:55:45 +0200

    add relation one customer to many orders

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrderLine.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrderLine.java
@@ -2,40 +2,47 @@ package guru.springframework.spring6restmvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.UUID;
 
-/**
- * Created by robertZ on 2024-01-30.
- */
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class Customer {
+public class BeerOrderLine {
+
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @JdbcTypeCode(SqlTypes.CHAR)
-    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", unique = true, updatable = false, nullable = false)
     private UUID id;
+
     @Version
-    private Integer version;
-    private String name;
+    private Long version;
 
-    @Column(length = 255)
-    private String email;
-
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime lastModifiedDate;
 
-    @OneToMany(mappedBy = "customer")
-    private Set<BeerOrder> beerOrders;
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    private Integer orderQuantity = 0;
+    private Integer quantityAllocated = 0;
+
 }
+
